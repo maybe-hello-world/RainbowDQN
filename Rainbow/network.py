be8854f5ab85a5f757f4ec96f31d4c638492f109
maybe-hello-world <maybe.hello.world@gmail.com>
2019-10-08 19:22:00 +0300

    dueling networks

--- Rainbow/network.py
+++ Rainbow/network.py
@@ -2,42 +2,58 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+import typing
+
 import numpy as np
 
 
-class DQN:
+class DDDQN(nn.Module):
+    # PyCharm problems
+    # https://youtrack.jetbrains.com/issue/PY-37601
+    def __call__(self, *inp, **kwargs) -> typing.Any:
+        return super().__call__(*inp, **kwargs)
+
     def __init__(self, inp_dim: int, out_dim: int, lr: float = 1e-3):
         """Input: (1, inp_dim)"""
+        super().__init__()
 
-        self.model = nn.Sequential(
-            nn.Linear(inp_dim, 32),
+        self.features = nn.Sequential(
+            nn.Linear(inp_dim, 128),
             nn.ReLU(),
-            nn.Linear(32, 32),
+            nn.Linear(128, 128),
+            nn.ReLU()
+        )
+
+        self.advantage = nn.Sequential(
+            nn.Linear(128, 64),
             nn.ReLU(),
-            nn.Linear(32, out_dim)
+            nn.Linear(64, out_dim)
+        )
+        self.value = nn.Sequential(
+            nn.Linear(128, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
         )
 
         self.lr = lr
-        self.opt = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.opt = optim.Adam(self.parameters(), lr=self.lr)
 
     def predict(self, state: np.ndarray, single=True):
         if single:
             state = np.expand_dims(state, axis=0)
-        return self.model(torch.from_numpy(state).float())
+        state = torch.from_numpy(state).float()
+        state = self.features(state)
+        value = self.value(state)
+        advantage = self.advantage(state)
+        return advantage + value - advantage.mean()
 
     def fit(self, state, y_true, weights=None):
-        y_pred = self.model(torch.from_numpy(state).float())
+        y_pred = self.predict(state, single=False)
         y_true = torch.from_numpy(y_true).float()
         loss = (y_pred - y_true).pow(2).sum(dim=1)
-        loss *= torch.from_numpy(np.array(weights)).float()
+        loss *= torch.from_numpy(weights).float()
 
         self.opt.zero_grad()
         loss.mean().backward()
         self.opt.step()
         return loss.detach().numpy()
-
-    def get_state(self):
-        return self.model.state_dict()
-
-    def set_state(self, state_dict):
-        self.model.load_state_dict(state_dict)
--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 sns.set(style="darkgrid")
 
-agent = Agent("CartPole-v1", batch_size=30, copy_every=10)
-results = agent.train(900)
-sns.lineplot(x=range(len(results)), y=results)
+agent = Agent("CartPole-v1", batch_size=50, copy_every=64)
+results, losses = agent.train(8192)
+
+fig, ax = plt.subplots(2, 1)
+sns.lineplot(x=range(len(results)), y=results, ax=ax[0])
+sns.lineplot(x=range(len(losses)), y=losses, ax=ax[1])
 plt.show()
 agent.show()
